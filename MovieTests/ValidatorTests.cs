88bd6f910cd28414bba7b94f8d4f8cf4e52f7882
David Mackie <dmackie@freewheel.com>
2023-02-10 11:47:51 +0000

    More tests on validation

--- MovieTests/ValidatorTests.cs
+++ MovieTests/ValidatorTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation.TestHelper;
+﻿using AutoFixture;
+using FluentValidation.TestHelper;
 using MovieAPI.Mediatr;
 using Xunit;
 
@@ -8,11 +9,13 @@ namespace MovieTests
     {
         private GetMoviesQuery _query;
         private GetMoviesQueryValidator _validator;
+        private Fixture _fixture;
 
 
         public ValidatorTests()
         {
             _validator = new GetMoviesQueryValidator();
+            _fixture = new Fixture();
         }
 
         [Fact]
@@ -23,6 +26,36 @@ namespace MovieTests
             result.ShouldHaveValidationErrorFor(x => x.Title);
             result.ShouldHaveValidationErrorFor(x => x.Year);
             result.ShouldHaveValidationErrorFor(x => x.Genre);
+        }
+
+        [Fact]
+        public void MovieQuery_AcceptsTitleOnly()
+        {
+            _query = new GetMoviesQuery() { Title = _fixture.Create<string>()};
+            var result = _validator.TestValidate(_query);
+            result.ShouldNotHaveValidationErrorFor(x => x.Title);
+            result.ShouldNotHaveValidationErrorFor(x => x.Year);
+            result.ShouldNotHaveValidationErrorFor(x => x.Genre);
+        }
+
+        [Fact]
+        public void MovieQuery_AcceptsYearOnly()
+        {
+            _query = new GetMoviesQuery() { Year = _fixture.Create<int>() };
+            var result = _validator.TestValidate(_query);
+            result.ShouldNotHaveValidationErrorFor(x => x.Title);
+            result.ShouldNotHaveValidationErrorFor(x => x.Year);
+            result.ShouldNotHaveValidationErrorFor(x => x.Genre);
+        }
+
+        [Fact]
+        public void MovieQuery_AcceptsGenreOnly()
+        {
+            _query = new GetMoviesQuery() { Genre = _fixture.Create<int>() };
+            var result = _validator.TestValidate(_query);
+            result.ShouldNotHaveValidationErrorFor(x => x.Title);
+            result.ShouldNotHaveValidationErrorFor(x => x.Year);
+            result.ShouldNotHaveValidationErrorFor(x => x.Genre);
         }
     }
 }
