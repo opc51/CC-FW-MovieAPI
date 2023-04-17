dfbe4511aeb945d11524db498e5b4d1ad6cf3dc9
David Mackie <dmackie@freewheel.com>
2023-04-17 17:27:17 +0100

    First problem found

---
+++ MovieTests/ConsumerTests/APIClient.cs
@@ -0,0 +1,46 @@
+﻿using MovieAPI.Models.Domain;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MovieTests.ConsumerTests
+{
+    public class ApiClient
+    {
+        private readonly HttpClient client;
+
+        public ApiClient()
+        {
+            client = new HttpClient { BaseAddress = new Uri("https://localhost:44365/") };
+        }
+
+        public async Task<List<Movie>> GetMovies()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "api//Movies?Title=super");
+            request.Headers.Add("Accept", "application/json");
+
+            var response = await client.SendAsync(request);
+
+            var content = await response.Content.ReadAsStringAsync();
+            var status = response.StatusCode;
+
+            string reasonPhrase = response.ReasonPhrase;
+
+            request.Dispose();
+            response.Dispose();
+
+            if (status == HttpStatusCode.OK)
+            {
+                return !string.IsNullOrEmpty(content)
+                           ? JsonConvert.DeserializeObject<List<Movie>>(content)
+                           : null;
+            }
+
+            throw new Exception(reasonPhrase);
+        }
+    }
+}
---
+++ MovieTests/ConsumerTests/APITests.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using PactNet;
+using Xunit.Abstractions;
+using Xunit;
+using System.Net.Http;
+using System.Net;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using PactNet.Matchers;
+
+namespace MovieTests.ConsumerTests
+{
+    public class ApiTests
+    {
+        private IPactBuilderV3 pact;
+        private readonly ApiClient ApiClient;
+        private readonly int port = 9000;
+        private readonly List<object> products;
+
+        public ApiTests(ITestOutputHelper output)
+        {
+            var Config = new PactConfig
+            {
+                PactDir = Path.Join("..", "..", "..", "..", "..", "pacts"),
+                Outputters = new[] { new XUnitOutput(output) },
+                DefaultJsonSettings = new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                }
+            };
+
+            pact = Pact.V3("ApiClient", "MovieService", Config).WithHttpInteractions();
+            ApiClient = new ApiClient();
+        }
+
+        [Fact]
+        public async void GetAllMovies()
+        {
+            // Arange
+            pact.UponReceiving("A valid request for all movies")
+                    .Given("movies exist")
+                    .WithRequest(HttpMethod.Get, "/api/movies?Title=super")
+                .WillRespond()
+                    .WithStatus(HttpStatusCode.OK)
+                    .WithHeader("Content-Type", "application/json; charset=utf-8")
+                    .WithJsonBody(new TypeMatcher(products));
+
+            await pact.VerifyAsync(async ctx => {
+                var response = await ApiClient.GetMovies();
+                Assert.Equal(5, response.Count);
+            });
+        }
+    }
+}
